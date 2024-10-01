28433bcc8c51ca3ce8e300fbf8cf356aeca479f8
emesonmikael <emesonmikael@gmail.com>
2024-09-30 21:13:12 -0300

    l,mn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import M3UPlayer from "./M3UPlayer";
 
 function App() {
-  const m3uUrl = "http://localhost:3000/canais.m3u"; // Seu link .m3u
+  const m3uUrl = "http://localhost:3000/Marvel.m3u"; // Seu link .m3u
 
   return (
     <div>
