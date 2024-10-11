14d521cfd4bd2348b75f5e3896e8997ded562e21
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-10-11 14:45:21 -0300

    ja

--- src/data/chanels.js
+++ src/data/chanels.js
@@ -1,6 +1,6 @@
 const channels = [
     {
-      name: "A FAZENDA PRINCIPAL",
+      name: "A FAZENDA PRINCIP1AL",
       logo: "",
       url: "http://cdn-br.in/978460358/473005646/628508.ts",
       group: "Canais || A Fazenda 16"
