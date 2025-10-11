4c61697e85629a5533aa0e7d1ffb7633022f1740
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2025-10-11 10:55:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css'
 import reportWebVitals from './reportWebVitals';
 //import conteudo2 from './conteudo2';
 //import App from './conteudo2';
-import App from './App';
+//import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
