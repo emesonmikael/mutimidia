97edbaacbe2eee24f892ba5d431ff256a17cba5c
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2025-10-19 14:49:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import M3UPlayer from "./M3UPlayer";
 
 function App() {
-  const m3uUrl = "https://backend-aejq.vercel.app/proxy/ssiptv.php?username=220789&password=989122&ssiptv&output=mpegts"; // Seu link .m3u
+  const m3uUrl = "https://proxy-server-eight-omega.vercel.app/proxy/ssiptv.php?username=220789&password=989122&ssiptv&output=mpegts"; // Seu link .m3u
 
   return (
     <div>
